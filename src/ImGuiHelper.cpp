df66996ef48c6c9d9b624eeee9939fb704a12976
AdamYuan <y13916619121@126.com>
2020-10-31 14:43:46 +0800

    Fix MSVC compile error

--- src/ImGuiHelper.cpp
+++ src/ImGuiHelper.cpp
@@ -22,7 +22,8 @@ namespace ImGui {
 		window->DrawList->PathClear();
 
 		int num_segments = 30;
-		int start = abs(ImSin(g.Time * 1.8f) * (num_segments - 5));
+		int start = ImSin(g.Time * 1.8f) * float(num_segments - 5);
+		start = start < 0 ? -start : start; //abs
 
 		const float a_min = IM_PI * 2.0f * ((float) start) / (float) num_segments;
 		const float a_max = IM_PI * 2.0f * ((float) num_segments - 3) / (float) num_segments;
