5cd518c80b1f39b27e91747854d7b522fbf8d327
AdamYuan <y13916619121@126.com>
2021-03-27 00:33:17 +0800

    use rgba32 font texture in ImGuiRenderer

--- src/myvk/Instance.cpp
+++ src/myvk/Instance.cpp
@@ -32,9 +32,9 @@ std::shared_ptr<Instance> Instance::CreateWithGlfwExtensions(bool use_validation
 	if (use_validation_layer && debug_callback) {
 		VkDebugUtilsMessengerCreateInfoEXT debug_info = {};
 		debug_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-		debug_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
-		                             VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-		                             VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		debug_info.messageSeverity =
+		    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+		    VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT;
 		debug_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
 		                         VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
 		                         VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
