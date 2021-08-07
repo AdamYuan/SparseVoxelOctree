42573022c390542d5caef1b496c026a3914bafde
AdamYuan <y13916619121@126.com>
2021-08-07 19:46:21 +0800

    Correct ImGui blend

--- src/ImGuiRenderer.cpp
+++ src/ImGuiRenderer.cpp
@@ -235,8 +235,8 @@ void ImGuiRenderer::create_pipeline(const std::shared_ptr<myvk::RenderPass> &ren
 		color_blend_attachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
 		color_blend_attachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
 		color_blend_attachment.colorBlendOp = VK_BLEND_OP_ADD;
-		color_blend_attachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
-		color_blend_attachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+		color_blend_attachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+		color_blend_attachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
 		color_blend_attachment.alphaBlendOp = VK_BLEND_OP_ADD;
 		color_blend_attachment.colorWriteMask =
 		    VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
